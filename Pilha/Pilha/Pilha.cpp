fd55838d754670529bed59f1e2eff176856efdad
lab53 <lab53@LAB56M13>
2023-04-18 12:10:57 -0300

    função push e pop

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -14,6 +14,7 @@ void menu();
 void inicializar();
 void pop();
 void push();
+void exibirElementos();
 //--------------------------
 
 
@@ -25,14 +26,15 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 4) {
+	while (op != 5) {
 		system("cls"); // somente no windows
 		cout << "Menu Pilha";
 		cout << endl << endl;
 		cout << "1 - Inicializar Pilha \n";
 		cout << "2 - Inserir elemento (Push) \n";
 		cout << "3 - Remover elementos (Pop) \n";
-		cout << "4 - Sair \n";
+		cout << "4 - exibirElementos \n";
+		cout << "5 - sair \n";
 
 
 		cout << "Opcao: ";
@@ -46,7 +48,9 @@ void menu()
 			break;
 		case 3: pop();
 			break;
-		case 4:
+		case 4: exibirElementos();
+			break;
+		case 5 :
 			return;
 		default:
 			break;
@@ -87,13 +91,30 @@ void push()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
+	novo->prox = topo;
+	topo = novo;
 
 }
 
 void pop()
 {
-
-	
-
+	NO* aux = topo;
+	topo = topo->prox;
+	free(aux);
 }
 
+void exibirElementos()
+{
+	if (topo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = topo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->prox;
+		}
+	}
+}
